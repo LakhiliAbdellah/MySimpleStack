8232d195edfb60e1e99511aa8c7b955b2e70d38e
abdellah <abdellah.lakhili.pro@gmail.com>
2016-02-24 11:11:23 +0100

    commit class

---
+++ src/main/java/stackControl/Item.java
@@ -0,0 +1,14 @@
+package stackControl;
+
+/**
+ * Created by 21207071 on 24/02/2016.
+ */
+public class Item {
+    private int val;
+    private String nom;
+
+    public Item(int i,String n){
+        this.val=i;
+        n=nom;
+    }
+}
---
+++ src/main/java/stackControl/SimpleStackImpl.java
@@ -0,0 +1,36 @@
+package stackControl;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Created by 21207071 on 24/02/2016.
+ */
+public class SimpleStackImpl implements SimpleStack {
+    Stack<Item> items=new Stack<Item>();
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return items.size();
+    }
+
+    @Override
+    public void push(Item item) {
+        items.push(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return items.peek();
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return items.pop();
+    }
+}
